d922076723fbc0ce157a0aa35d4659de26fc7f3c
gumbi <gusn9719@gmail.com>
2024-10-24 20:34:12 +0900

    feat(constants): 입력, 결과, 오류 메시지 상수 추가
    - 사용자 입력 요청 메시지 상수 추가 (PROMPTS)
    - 결과 출력 메시지 상수 추가 (RESULTS)
    - 오류 메시지 상수 추가 (ERROR_MESSAGES)

---
+++ src/constants/constants.js
@@ -0,0 +1,15 @@
+export const PROMPTS = {
+  ASK_CAR_NAMES:
+    '경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n',
+  ASK_TRY_COUNT: '시도할 횟수는 몇 회인가요?\n',
+};
+
+export const RESULTS = {
+  WINNER_ANNOUNCEMENT: '최종 우승자 : ',
+};
+
+export const ERROR_MESSAGES = {
+  INVALID_INPUT: '[ERROR] 올바르지 않은 입력입니다.',
+  INVALID_CAR_NAME: '[ERROR] 자동차 이름을 올바르게 입력해 주세요.',
+  INVALID_TRY_COUNT: '[ERROR] 시도할 횟수를 옳바르게 입력해 주세요.',
+};
